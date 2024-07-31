13a6fe65d43bc3d07bc7930bc14e3f7a61a3bb8f
Kacper Czajka <czajkaakacper@gmail.com>
2024-07-31 21:23:09 +0200

    problems with routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+// import Login from './components/Login';
+// import Signup from './components/Signup';
+// import Main from './components/Main';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <>
+                <Navbar bg="dark" data-bs-theme="dark">
+                    <Container>
+                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+            </>
+        </div>
+    );
 }
 
 export default App;
+
+
+
+
+// <Routes>
+//   <Route path='/' element={<Navigate replace to="/main" />} />
+//   <Route path="/signup" element={<Signup />} />
+//   <Route path="/main" element={<Main />} />
+//   <Route path="/login" element={<Login />} />
+// </Routes>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+//import App from './components/Main/App.js';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +15,15 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// import Login from './components/Login';
+// import Signup from './components/Signup';
+// import Main from './components/Main';
+
+// <Routes>
+//   <Route path='/' element={<Navigate replace to="/main" />} />
+//   <Route path="/signup" element={<Signup />} />
+//   <Route path="/main" element={<Main />} />
+//   <Route path="/login" element={<Login />} />
+// </Routes>
