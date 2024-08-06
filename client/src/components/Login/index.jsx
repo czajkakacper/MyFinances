af3de52c8bdbbbee9620ea008ab430eb5ced77c5
Kacper Czajka <K.Czajka@maspex.com>
2024-08-06 10:59:47 +0200

    signup/login changes

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,17 +1,35 @@
-import { Button, Container, Nav, Navbar, Modal, Form, FormControl } from "react-bootstrap";
+import {
+  Button,
+  Container,
+  Nav,
+  Navbar,
+  Modal,
+  Form,
+  FormControl,
+} from "react-bootstrap";
 import { useState } from "react";
-import axios from "axios"
-import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import "./styles.css";
 import "../../App.js";
 import "../../index.js";
 
-
 const Login = () => {
-    const [show, setShow] = useState(true);
+  const [show, setShow] = useState(true);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState(""); // Stan dla wiadomości sukcesu
+  const navigate = useNavigate();
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const handleChange = ({ currentTarget: input }) => {
+    setFormData({ ...formData, [input.name]: input.value });
+  };
 
   return (
     <div className="App">
@@ -21,28 +39,37 @@ const Login = () => {
             <h3>
               <i>My Finances</i>
             </h3>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto"></Nav>
-              <Button variant="outline-success" onClick={handleShow}>ZALOGUJ</Button>
-            </Navbar.Collapse>
           </Container>
         </Navbar>
 
         <Modal show={show} onHide={handleClose} centered>
           <Modal.Header closeButton>
-            <Modal.Title className="modal-title-center">Zaloguj się</Modal.Title>
+            <Modal.Title className="modal-title-center">
+              Zaloguj się
+            </Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <Form>
-              <Form.Group controlId="formBasicEmail">
+              <Form.Group controlId="formBasicEmail" className="mt-3">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="jankowalski@gmail.com"
+                  onChange={handleChange}
+                  value={formData.email}
+                />
               </Form.Group>
 
               <Form.Group controlId="formBasicPassword" className="mt-3">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  type="password"
+                  name="password"
+                  placeholder="********"
+                  onChange={handleChange}
+                  value={formData.password}
+                />
               </Form.Group>
 
               <Button variant="primary" type="submit" className="mt-3 w-100">
@@ -55,14 +82,16 @@ const Login = () => {
             <div className="text-center">
               <p>Nie masz konta?</p>
               <Link to="/register">
-                <Button variant="outline-primary" className="w-100">REJESTRACJA</Button>
+                <Button variant="outline-primary" className="w-100">
+                  REJESTRACJA
+                </Button>
               </Link>
             </div>
           </Modal.Body>
         </Modal>
       </>
     </div>
-    )
+  );
 };
 
 export default Login;
---
+++ client/src/components/Signup/index.jsx
@@ -0,0 +1,127 @@
+import {
+  Button,
+  Container,
+  Nav,
+  Navbar,
+  Modal,
+  Form,
+  FormControl,
+} from "react-bootstrap";
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import "./styles.css";
+import "../../App.js";
+import "../../index.js";
+
+const Signup = () => {
+  const [show, setShow] = useState(true);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState(""); // Stan dla wiadomości sukcesu
+  const navigate = useNavigate();
+  
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleChange = ({ currentTarget: input }) => {
+    setFormData({ ...formData, [input.name]: input.value });
+  };
+
+  return (
+    <div className="App">
+      <>
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+          <Container>
+            <h3>
+              <i>My Finances</i>
+            </h3>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto"></Nav>
+              <Button variant="outline-success" onClick={handleShow}>
+                ZALOGUJ
+              </Button>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Modal show={show} onHide={handleClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title className="modal-title-center">
+              Zaloguj się
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="formBasicFirstName">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="firstName"
+                  placeholder="Jan"
+                  onChange={handleChange}
+                  value={formData.firstName}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicLastName" className="mt-3">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="lastName"
+                  placeholder="Kowalski"
+                  onChange={handleChange}
+                  value={formData.lastName}
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail" className="mt-3">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="jankowalski@gmail.com"
+                  onChange={handleChange}
+                  value={formData.email}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword" className="mt-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  placeholder="********"
+                  onChange={handleChange}
+                  value={formData.password}
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit" className="mt-3 w-100">
+                REJESTRACJA
+              </Button>
+            </Form>
+
+            <div className="horizontal-divider my-4"></div>
+
+            <div className="text-center">
+              <p>Masz już konto?</p>
+              <Link to="/register">
+                <Button variant="outline-primary" className="w-100">
+                  ZALOGUJ
+                </Button>
+              </Link>
+            </div>
+          </Modal.Body>
+        </Modal>
+      </>
+    </div>
+  );
+};
+
+export default Signup;
